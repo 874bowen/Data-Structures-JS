dd9c2ff7276e37469e5a198585d922640ef77251
Ivan Bowen <bowenivan16@gmail.com>
2022-08-06 16:40:10 +0300

    create the reverse word by popping the letters from letters stack

--- stacks.js
+++ stacks.js
@@ -13,3 +13,7 @@ for (let i = 0; i <= word.length; i++){
     letters.push(word[i]);
 }
 
+// add the letters to reverse word by popping them
+for (let i = 0; i <= letters.length; i++){
+    rword += letters.pop();
+}
