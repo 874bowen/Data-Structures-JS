0e80df156a0bc97007c944a62b50bb75cb1d015e
Ivan Bowen <bowenivan16@gmail.com>
2022-08-09 09:17:50 +0300

    initializing sets

---
+++ sets.js
@@ -0,0 +1,15 @@
+/* SETS 
+    - an array with no duplicate items and have no particular order
+    - used in checking for presence of an element in an array
+    - has() - check presence
+    - values() - retrieve all values
+    - add() - push to collection
+    - remove() - remove an element --delete--
+    - size() 
+    - union() - combine two sets
+    - intersection() - intersection of two sets
+    - difference() - all elements in one set but not in another
+    - subset() - returns true or false
+*/
+
+
