bb5e7a6ac69d5d2c1af6ded3e7127adbbf9e2fd0
Ivan Bowen <bowenivan16@gmail.com>
2022-08-19 19:23:02 +0300

    add a small demo of how h.o.f works

--- headfirst/battleship/higherOrderFxn.js
+++ headfirst/battleship/higherOrderFxn.js
@@ -1,1 +1,9 @@
 /* higher order functions are functions which take functions as arguments or return functions as return value */
+
+function x() {
+    console.log("I am Bowen. God loves me");
+}
+function y(x) {
+    x();
+}
+y();
