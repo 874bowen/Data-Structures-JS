981ac48a59a9fac6cbc34cffe595ded08f629acd
Ivan Bowen <bowenivan16@gmail.com>
2022-08-19 20:04:03 +0300

    add the beauty of higher order function - modularity & reusability

--- headfirst/battleship/higherOrderFxn.js
+++ headfirst/battleship/higherOrderFxn.js
@@ -1,34 +1,29 @@
 /* higher order functions are functions which take functions as arguments or return functions as return value */
 
 
+// STEPS IN WRITING HIGHER ORDER FUNCTIONS
+// 1) Remove the code which is changing for our case the area, diameter and circumference calculations
+// 2) Ensure there are no redundancies
+// 3) Create a general function that takes the removed function as param
+
+const area = (radius) => { return Math.PI * radius * radius; }
+
+const circumference = (radius) => { return 2 * Math.PI * radius; }
+
+const diameter = (radius) => { return 2 * radius; }
+
 // calculate area
 const radius = [3, 1, 2, 4];
 
-const calculateArea = (radii) => {
+// the general calculate fxn
+const calculate = (radii, logic) => {
     const output = [];
     for (let i = 0; i < radii.length; i++) {
-        output.push(Math.PI * radii[i] * radii[i]);        
+        output.push(logic(radius[i]));        
     }
     return output;
 } 
-console.log(calculateArea(radius));
 
-// calculate circumference 
-const calculateCircumference = (radii) => {
-    const output = [];
-    for (let i = 0; i < radii.length; i++) {
-        output.push(2 * Math.PI * radii[i]);        
-    }
-    return output;
-} 
-console.log(calculateCircumference(radius));
-
-// calculate diameter 
-const calculateDiameter = (radii) => {
-    const output = [];
-    for (let i = 0; i < radii.length; i++) {
-        output.push(2 * radii[i]);        
-    }
-    return output;
-} 
-console.log(calculateDiameter(radius));
+console.log(calculate(radius, area));
+console.log(calculate(radius, circumference));
+console.log(calculate(radius, diameter));
