95e8934b1d0fa5e9ae4e7479508a1615b0b2d93f
Ivan Bowen <bowenivan16@gmail.com>
2022-08-23 22:26:09 +0300

    add intro to reduceFxn in JS

---
+++ headfirst/battleship/reduceFxn.js
@@ -0,0 +1,7 @@
+const arr = [5, 1, 3, 2, 6]
+
+// reduce does not reduce anything😆
+// it is used when you have to take the whole array come with one or a SINGLE value out of them
+
+// e.g., sum or max number
+
