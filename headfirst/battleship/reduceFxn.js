36a47f672ffdd8fe69edb3e22ad0a00d5b193870
Ivan Bowen <bowenivan16@gmail.com>
2022-08-23 22:50:50 +0300

    add reduce function to get sum

--- headfirst/battleship/reduceFxn.js
+++ headfirst/battleship/reduceFxn.js
@@ -1,7 +1,27 @@
-const arr = [5, 1, 3, 2, 6]
+const arr = [5, 1, 3, 10, 2, 6]
 
 // reduce does not reduce anything😆
 // it is used when you have to take the whole array come with one or a SINGLE value out of them
 
 // e.g., sum or max number
+
+// non-functional way
+function findSum(arr) {
+     let sum = 0;
+     for (let i = 0; i < arr.length; i++) {
+          sum += arr[i];   
+     }
+     return sum;
+}
+console.log(findSum(arr));
+
+// reduce runs accross each value
+// current is the values i.e., arr[i]
+// accumulator used to accumulate the values i.e., sum
+// second argument to reduce is the initial value of the accumulator
+const output = arr.reduce(function(acc, curr){
+     acc += curr;
+     return acc;
+}, 0);
+console.log(output);
 
