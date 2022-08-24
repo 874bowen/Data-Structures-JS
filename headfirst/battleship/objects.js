a20bd46682de5bf595f46ef5806c51737e74d9b2
Ivan Bowen <bowenivan16@gmail.com>
2022-08-24 07:34:58 +0300

    add rules used whwen creating an object in JS

---
+++ headfirst/battleship/objects.js
@@ -0,0 +1,18 @@
+// sort of a group of name:  property value 
+// Rules
+// enclose object in curly braces
+// separate the name and property value using a colon
+// name can be string if has spaces enclose it with quotes
+// you can not have two or more properties of same name
+// separate each property name and value with a comma 
+// Dont use comma after last property and name
+
+var chevy = { 
+     make: "Chevy",
+     model: "Bel Air",
+     year: 1957, 
+     color: "red",
+     passengers: 2,
+     "is convertible": false, // if has space sorround with quotes
+     mileage: 1021
+    };
