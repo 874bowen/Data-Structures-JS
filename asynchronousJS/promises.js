206304a19a564cc6d34e23f277e634ddae40bc96
Ivan Bowen <bowenivan16@gmail.com>
2022-08-27 17:42:47 +0300

    add promises

--- asynchronousJS/promises.js
+++ asynchronousJS/promises.js
@@ -27,4 +27,12 @@ function createPost(post) {
      });
 }
 
-createPost({ title: "Post Three", body: "This is post three"}).then(getPosts).catch(err => console.log(err));
+// createPost({ title: "Post Three", body: "This is post three"}).then(getPosts).catch(err => console.log(err));
+
+// Promise.all
+const promise1 = Promise.resolve("Hello World");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+
+const promise4 = fetch("https://someurl........").then(res => res.json);
+Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
