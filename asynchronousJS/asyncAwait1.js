a720655e2c36677e652223d9c14e7053b3b828f6
Ivan Bowen <bowenivan16@gmail.com>
2022-08-29 23:02:23 +0300

    add async await part one

--- asynchronousJS/asyncAwait1.js
+++ asynchronousJS/asyncAwait1.js
@@ -1,18 +1,31 @@
+let stocks = {
+     Fruits: ["strawberry", "grapes", "banana", "apple"],
+     liquid: ["water", "ice"],
+     holder: ["cone", "cup", "stick"],
+     toppings: ["chocolate", "peanuts"]
+};
+
 let is_shop_open = true;
 
-let order = (time, work) => {
+let toppings_choice = () => {
 
      return new Promise((resolve, reject) => {
-          if(is_shop_open){
-               resolve(work());
-          } else{
-               reject(console.log("Err"));
-          }
+          setTimeout(()=>{
+               resolve(console.log("Which toppings do you like ? "));
+          }, 3000);
      });
 };
-// .then().then().then().catch().finally()
-// code above is same as code below
 
-async function order (){
-
+async function kitchen (){
+     console.log(" STEP 1 ");
+     console.log(" STEP 2 ");
+     console.log(" STEP 3 ");
+     await toppings_choice();
+     console.log(" STEP 4 ");
+     console.log(" STEP 5 ");
 }
+kitchen();
+
+console.log("Doing dishes continues ... ");
+console.log("Cleaning the tables ... ");
+console.log("Taking other orders ... ");
