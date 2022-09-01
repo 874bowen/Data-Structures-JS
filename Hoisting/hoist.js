daded58e47918a7ac195f46d5b5e7bea1333fc41
Ivan Bowen <bowenivan16@gmail.com>
2022-09-01 22:05:25 +0300

    add an arrow function & it behaves like a variable

--- Hoisting/hoist.js
+++ Hoisting/hoist.js
@@ -1,7 +1,7 @@
 getName("Ivan");
 console.log(x);
 console.log(getName);
-
+arrow_function
 
 var x = 7;
 
@@ -9,3 +9,6 @@ function getName(name) {
      console.log(name +" learning JS with Namaste JavaScript!!");
 }
 
+var arrow_function = () =>{
+     console.log("This is an arow function");
+}
