ffb6a52b75c7cb5090adb783eb59b2522916d134
Ivan Bowen <bowenivan16@gmail.com>
2022-09-05 21:59:20 +0300

    add trees in JavaScript

---
+++ data structures/trees.js
@@ -0,0 +1,95 @@
+class Node {
+     constructor (data, left=null, right=null){
+          this.data = data;
+          this.left = left;
+          this.right = right;
+     }
+}
+
+class BST {
+     constructor(){
+          this.root = null;
+     }
+     add(data){
+          const node = this.root;
+          if (node === null){
+               this.root = new Node(data);
+               return;
+          } else {
+               const searchTree = function(node){
+                    if (data < node.data){
+                         if(node.left === null){
+                              node.left = new Node(data);
+                               return;
+                         } else if(node.left !== null){
+                              return searchTree(node.left);
+                         }
+                    } else if( data > node.data){
+                         if (node.right === null){
+                              node.right = new Node(data);
+                              return;
+                         } else if (node.right !== null){
+                              return searchTree(node.right);
+                         }
+                    } else{
+                         return null;
+                    }
+               };
+               return searchTree(node);
+          }
+     }
+     findMin(){
+          let current = this.root;
+          for( ;current.left !== null; ){
+               current = current.left;
+          }
+          return current.data;
+     }
+     findMax(){
+          let current = this.root;
+          for ( ;current.right !== null; ){
+               current = current.right;
+          }
+          return current.data;
+     }
+     find(data){
+          let current = this.root;
+          for ( ;current.data !== data; ){
+               if (data < current.data){
+                    current = current.left;
+               } else if (data > current.data){
+                    current = current.right;
+               } 
+               if (current === null){
+                    return null
+               }
+          }
+          return current;
+     }
+     isPresent(data){
+          let current = this.root;
+          for( ;current; ){
+               if (current.data === data){
+                    return true
+               }
+               if (data < current.data){
+                    current = current.left;
+               } else {
+                    current = current.right;
+               }
+          }
+          return false;
+     }
+}
+
+let newBst = new BST();
+
+newBst.add(10);
+newBst.add(20);
+newBst.add(30);
+newBst.add(25);
+newBst.add(19);
+
+console.log(newBst.isPresent(20));
+console.log(newBst.findMax());
+console.log(newBst.findMin());
