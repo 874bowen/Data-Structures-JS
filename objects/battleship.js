0055b7b4855dbf161b4039181d66e915e5c225e1
Ivan Bowen <bowenivan16@gmail.com>
2022-09-06 07:10:37 +0300

    add controller to battleship

--- objects/battleship.js
+++ objects/battleship.js
@@ -66,3 +66,43 @@ model.fire("44");
 model.fire("12");
 model.fire("11");
 model.fire("10");
+
+// CONTROLLER
+var controller = {
+     guesses: 0,
+     processGuess: function(guess) {
+          var location = parseGuess(guess);
+          if (location) {
+               this.guesses++;
+               var hit = model.fire(location);
+          }
+     }
+};
+
+var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+function parseGuess(guess) {
+     if (guess === null || guess.length !== 2) {
+          alert("Oops, please enter a letter and a number on the board.");
+     }  else {
+          var firstChar = guess.charAt(0);
+          var row = alphabet.indexOf(firstChar);
+          var column = guess.charAt(1);
+
+          if (isNaN(row) || isNaN(column)) {
+               alert("Oops, that isn't on the board.");
+          } else if (row < 0 || row >= model.boardSize ||
+          column < 0 || column >= model.boardSize) {
+               alert("Oops, that's off the board!");
+          } else {
+               return row + column;
+          }
+     }
+     return null;
+}
+
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
