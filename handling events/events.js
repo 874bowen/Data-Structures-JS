03295c5d619e192434d34b8a059c650423ec7e30
Ivan Bowen <bowenivan16@gmail.com>
2022-09-12 19:09:47 +0300

    add more of events

--- handling events/events.js
+++ handling events/events.js
@@ -3,12 +3,22 @@ function showAnswer(e){
      var name = image.id;
      name = "./assets/" + name + ".jpg";
      image.src = name;
+     // setTimeout(reblur, 2000, image);
+}
+
+function reblur(e) {
+     console.log(e.timestamp);
+     var image = e.target;
+     var name = image.id;
+     name = "./assets/" + name + "blur.jpg";
+     image.src = name;
 }
 
 function init(){
      var images = document.getElementsByTagName("img");
      for (var i = 0; i < images.length; i++) {
-          images[i].onclick = showAnswer;
+          images[i].onmouseover = showAnswer;
+          images[i].onmouseout= reblur;
      }
 }
 
