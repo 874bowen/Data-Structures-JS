cda142be698a5aef6930efe5691f8af6e0d015ad
Ivan Bowen <bowenivan16@gmail.com>
2022-09-20 21:08:58 +0300

    add the users data

---
+++ recursion/recur.js
@@ -0,0 +1,14 @@
+let user = {
+     name: "Ivan",
+     address: {
+          personal: {
+               city: "Eldoret",
+               area: "Pioneer"
+          },
+          office: {
+               city: "Nairobi",
+               area: "Westlands"
+          }
+     },
+     phone: "0791440095"
+}
