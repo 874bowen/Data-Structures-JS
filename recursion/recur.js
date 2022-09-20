f15a63371f32e09a4ebc2a2fd1feaafacc63bef8
Ivan Bowen <bowenivan16@gmail.com>
2022-09-20 21:18:57 +0300

    add the recursion function

--- recursion/recur.js
+++ recursion/recur.js
@@ -7,8 +7,27 @@ let user = {
           },
           office: {
                city: "Nairobi",
-               area: "Westlands"
+               area: "Westlands",
+               site: {
+                    remote: false,
+                    urban: true
+               }
           }
      },
      phone: "0791440095"
 }
+
+let finalObj = {};
+let magic = (obj, parent) => {
+     for(let key in obj){
+          if(typeof(obj[key]) === 'object'){
+               magic(obj[key], parent+"_"+key);
+          } else{
+               finalObj[parent+"_"+key] = obj[key];
+          }
+     }
+     return finalObj;
+}
+
+let magic1 = magic(user, "user");
+console.log(magic1);
