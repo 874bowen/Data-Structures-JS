09cde3272c6c66395d67a25a86f09961c1b4c3e7
Ivan Bowen <bowenivan16@gmail.com>
2022-09-21 07:41:30 +0300

    the apply method which takes the arguments in an array

--- shorts/call_apply_bind.js
+++ shorts/call_apply_bind.js
@@ -3,15 +3,21 @@ let name1 = {
      sname: "Bowen"
 }
 
-printFullName = function(){
-     console.log(this.fname+" "+ this.sname);
+// when we have parameters arguments are passed second after the reference object
+printFullName = function(hometown, state){
+     console.log(this.fname+" "+ this.sname +" from " + hometown + ", " + state);
 }
 
-printFullName.call(name1);
+printFullName.call(name1, "Eldoret", "Kenya");
 
 let name2 = {
      fname: "Ian",
      sname: "Kiprono", 
 }
-// function borrowing
-printFullName.call(name2);
+// function borrowing with call method => 
+printFullName.call(name2, "Nairobi", "Kenya");
+
+// apply method => for apply instead of passing the arguments individually you can pass an array of arguments not individually like call
+
+printFullName.apply(name2, ["Nairobi", "Kenya"]);
+
