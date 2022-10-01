dd5a4fd56a1e6860e9f16aa10d9d4833f38e68a4
Ivan Bowen <bowenivan16@gmail.com>
2022-10-01 20:36:05 +0300

    liberated functions - function declaration and function expression

---
+++ funtional programming/liberated_functions.js
@@ -0,0 +1,19 @@
+var winner = function() { alert("WINNER!") };
+var loser = function() { alert("LOSER!") };
+// let's test as a warm up
+winner();
+// let's assign to other variables for practice
+var a = winner;
+var b = loser;
+var c = loser;
+a();
+b();
+// now let's try your luck with a shell game
+c = a;
+a = b;
+b = c;
+c = a;
+a = c;
+a = b;
+b = c;
+a();
