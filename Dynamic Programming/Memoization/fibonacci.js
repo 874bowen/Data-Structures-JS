4a2141b42a3e747f68ad4d052f6140391eca4ea0
Ivan Bowen <bowenivan16@gmail.com>
2022-10-03 20:28:39 +0300

    memoization - dynamic programming fibonacci sequence

--- Dynamic Programming/Memoization/fibonacci.js
+++ Dynamic Programming/Memoization/fibonacci.js
@@ -1,6 +1,9 @@
-const fib = (n) => {
+// memoization - reminder sort of 
+const fib = (n, memo = {}) => {
+     if (n in memo) return memo[n];
      if (n <= 2) return 1;
-     return fib(n-1) + fib(n-2);
+     memo[n] = fib(n-1, memo) + fib(n-2, memo);
+     return memo[n];
 } 
 console.log(fib(6));
 console.log(fib(40));
