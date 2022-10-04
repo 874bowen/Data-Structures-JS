389aae732bf10e39e8f8c552db54f9459ec2f66a
Ivan Bowen <bowenivan16@gmail.com>
2022-10-04 08:16:26 +0300

    optimized solution using memoization O(n*m) time

--- Dynamic Programming/Memoization/gridTraveller.js
+++ Dynamic Programming/Memoization/gridTraveller.js
@@ -1,7 +1,9 @@
-function gridTraveller(m, n) {
+function gridTraveller(m, n, memo = {}) {
+     let key = m + "," + n;
+     if (key in memo) return memo[key];
      if (m === 1 && n === 1) return 1;
      if (m === 0 || n === 0) return 0;
-     return gridTraveller(m-1, n) + gridTraveller(m, n-1);
+     return memo[key] = gridTraveller(m-1, n, memo) + gridTraveller(m, n-1, memo);
 }
 
 console.log(gridTraveller(1, 1))
