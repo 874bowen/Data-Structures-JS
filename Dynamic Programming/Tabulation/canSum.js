5d4fb3138b0fc9c17d212143a106afbda87f6976
Ivan Bowen <bowenivan16@gmail.com>
2022-10-13 22:07:15 +0300

    add canSum solution with tabulation

---
+++ Dynamic Programming/Tabulation/canSum.js
@@ -0,0 +1,17 @@
+const canSum = (targetSum, numbers) => {
+     const table = Array(targetSum + 1).fill(false);
+     table[0] = true;
+     for(let i = 0; i <= targetSum; i++){
+          if(table[i]){
+               for (let number of numbers){
+                    table[i+number] = true;
+               }
+          }
+     }
+     return table[targetSum];
+}
+console.log(canSum(7, [2, 3]))
+console.log(canSum(7, [5, 3, 4, 7]))
+console.log(canSum(7, [2, 4]))
+console.log(canSum(8, [2, 3, 5]))
+console.log(canSum(300, [7, 14]))
