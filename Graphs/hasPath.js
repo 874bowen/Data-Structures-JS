bfa303b17477e5802071cb57569096437fb10672
Ivan Bowen <bowenivan16@gmail.com>
2022-10-17 07:53:55 +0300

    has path solution bradth first search approach

--- Graphs/hasPath.js
+++ Graphs/hasPath.js
@@ -17,3 +17,15 @@ const has_path_dfs = (graph, src, dst) => {
 }
 
 console.log(has_path_dfs(graph, "a", "e"));
+
+const has_path_bfs = (graph, src, dst) => {
+     let queue = [src]; 
+     for( ;queue.length > 0; ){
+          let current = queue.shift();
+          if (current === dst) return true;
+          for (let neighbor of graph[current]) queue.push(neighbor);
+     }
+     return false;
+}
+
+console.log(has_path_bfs(graph, "a", "f"));
