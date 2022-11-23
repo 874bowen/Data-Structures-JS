431edda8d35e27c98c93adcad127deb5fa572d44
Ivan Bowen <bowenivan16@gmail.com>
2022-11-23 12:41:32 +0300

    prototypal inheritance in JavaScript

---
+++ ObjectVille/dogPrototype.js
@@ -0,0 +1,36 @@
+function Dog(name, breed, weight) {
+   this.name = name;
+   this.breed = breed;
+   this.weight = weight;
+}
+
+Dog.prototype.species = "Canine";
+
+Dog.prototype.bark = function () {
+   if (this.weight > 25) {
+      console.log(this.name + " says Woof!");
+   } else {
+      console.log(this.name + " says Yip!");
+   }
+};
+Dog.prototype.run = function () {
+   console.log("Run!");
+};
+Dog.prototype.wag = function () {
+   console.log("Wag!");
+};
+
+
+let fido = new Dog("Fido", "Mixed", 38);
+let fluffy = new Dog("Fluffy", "Poodle", 30);
+let spot = new Dog("Spot", "Chihuahua", 10);
+
+fido.bark();
+fido.run();
+fido.wag();
+fluffy.bark();
+fluffy.run();
+fluffy.wag();
+spot.bark();
+spot.run();
+spot.wag()
