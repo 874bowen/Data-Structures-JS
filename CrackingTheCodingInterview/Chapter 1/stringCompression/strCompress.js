9ab21e1638af1a0efe88a6dceb5eddab3a434f54
Ivan Bowen <bowenivan16@gmail.com>
2022-11-23 07:23:41 +0300

    use an array to reduce string concatenation O( n ^ 2 )

--- CrackingTheCodingInterview/Chapter 1/stringCompression/strCompress.js
+++ CrackingTheCodingInterview/Chapter 1/stringCompression/strCompress.js
@@ -1,17 +1,18 @@
 // String Compression: Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z). 
 
 const strCompress = (str) => {
-   let compressed = "";
+   let compressed = [];
    let count = 1;
    for (let i = 1; i <= str.length; i++){
       if (str[i] !== str[i-1]) {
-         compressed += str[i-1] + count;
+         compressed.push(str[i-1]);
+         compressed.push(count);
          count = 1;
          continue;
       }
       count++;
    }
-   return compressed;
+   return compressed.join("");
 } 
 
 // Test
