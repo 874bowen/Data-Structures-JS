a4a6058baa5bbf8fd3ec373faca2c1d542671f62
Ivan Bowen <bowenivan16@gmail.com>
2022-11-28 10:27:34 +0300

    check whether the object has own property or inherits

--- ObjectVille/Prototypal Inheritance/dogPrototype.js
+++ ObjectVille/Prototypal Inheritance/dogPrototype.js
@@ -29,7 +29,7 @@ let fluffy = new Dog("Fluffy", "Poodle", 30);
 let spot = new Dog("Spot", "Chihuahua", 10);
 // overriding the bark for spot
 
-spot.bark = function(){
+spot.bark = function () {
    console.log(this.name + " says WOOF!");
 }
 fido.bark();
@@ -43,7 +43,20 @@ spot.run();
 spot.wag();
 
 var barnaby = new Dog("Barnaby", "Basset Hound", 55);
-Dog.prototype.sit = function() {
- console.log(this.name + " is now sitting");
+Dog.prototype.sitting = false;
+Dog.prototype.sit = function () {
+   if (!this.sitting) {
+      // When we first check to see if Spot has his own sitting property we get false.
+      console.log("sitting " + this.hasOwnProperty("sitting"));
+      console.log(this.name + " is now sitting");
+      this.sitting = true;
+      // Then we set spot.sitting to true, adding this property to the spot instance.
+
+      console.log("sitting " + this.hasOwnProperty("sitting"));
+   } else {console.log(this.name + " is already sitting");}
 }
 barnaby.sit();
+barnaby.sit();
+console.log(spot.hasOwnProperty("name"));
+// But this call to hasOwnProperty returns false, because the fido instance does not have a sitting property. That means the sitting property that fido uses is defined only in the prototype, and inherited by fido
+console.log(fido.hasOwnProperty("sitting"));
