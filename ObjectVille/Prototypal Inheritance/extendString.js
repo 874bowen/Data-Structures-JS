5459d5033f0c61ea9e63fcd3b5318f1a518ca4bb
Ivan Bowen <bowenivan16@gmail.com>
2022-12-12 09:37:03 +0300

    add cliche method to String prototype

---
+++ ObjectVille/Prototypal Inheritance/extendString.js
@@ -0,0 +1,16 @@
+String.prototype.cliche = function () {
+   let cliche = ["lock and load", "touch base", "open the kimono"];
+
+   for (let i = 0; i < cliche.length; i++){
+      let index = this.indexOf(cliche[i])
+      if (index >= 0) return true;
+   }
+   return false;
+}
+
+let sentences = ["i'll send my car around to pick you up.", "Let's touch base in the morning and see where we are.", "We don't want to open the kimono, we just want to inform them"];
+
+for (let i = 0; i < sentences.length; i++){
+   let phrase = sentences[i];
+   if (phrase.cliche()) console.log("CLICHE ALERT: " + phrase);
+}
