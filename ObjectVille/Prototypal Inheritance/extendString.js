419b21388603421e9ce0fc24d9df4145c372ee70
Ivan Bowen <bowenivan16@gmail.com>
2022-12-12 09:51:40 +0300

    add palidrome method to String prototype

--- ObjectVille/Prototypal Inheritance/extendString.js
+++ ObjectVille/Prototypal Inheritance/extendString.js
@@ -1,16 +1,21 @@
 String.prototype.cliche = function () {
    let cliche = ["lock and load", "touch base", "open the kimono"];
 
-   for (let i = 0; i < cliche.length; i++){
+   for (let i = 0; i < cliche.length; i++) {
       let index = this.indexOf(cliche[i])
       if (index >= 0) return true;
    }
    return false;
 }
 
+String.prototype.palindrome = function () {
+   var r = this.split("").reverse().join("");
+   return (r === this.valueOf());
+}
+console.log("racecar".palindrome());
 let sentences = ["i'll send my car around to pick you up.", "Let's touch base in the morning and see where we are.", "We don't want to open the kimono, we just want to inform them"];
 
-for (let i = 0; i < sentences.length; i++){
+for (let i = 0; i < sentences.length; i++) {
    let phrase = sentences[i];
    if (phrase.cliche()) console.log("CLICHE ALERT: " + phrase);
 }
